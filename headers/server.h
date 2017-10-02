c712e561c05f1e95db4dc678ffca95bca120fe88
Michael <michael@michael-VirtualBox.(none)>
2017-10-03 09:45:45 +1000

    Updated server.h

--- headers/server.h
+++ headers/server.h
@@ -1,6 +1,41 @@
-// Includes
-#include <stdio.h>
-#include <stdlib.h>
+/*
+  	   CAB403 Assignment
+ 
+      By Michael Bell & Kyle Moyle
+*/
 
-// Variables
-#define DEFAULT_PORT	12345
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <sys/wait.h> 
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>      
+
+#define DEFAULT_PORT    12345
+#define BUF_SIZE        500
+
+
+const char *WELCOME_MESSAGE = "\n"
+                              "=============================================\n"
+                              "\n"
+			      "\n"
+                              "Welcome to the Online Hangman Gaming System\n"
+                              "\n"
+			      "\n"
+                              "=============================================\n"
+                              "\n";
+
+const char *LOGIN = "You are required to logon with your username and password\n";
+const char *USERNAME = "\nPlease enter your username-->";
+const char *PASSWORD = "\nPlease enter your password-->";
+
+
+
+
