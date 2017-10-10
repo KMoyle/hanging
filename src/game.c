a4bdc23df1ce992e1cd1dd7e1efec6469e46a658
Michael <michael@michael-VirtualBox.(none)>
2017-10-10 14:32:10 +1000

    Trying to implement random word function

--- src/game.c
+++ src/game.c
@@ -30,12 +30,27 @@ int calculate_num_guesses(int words_length){
 	int case1 = words_length + 10;
 	int case2 = 26;
 	
-	if case1 < case2{
+	if (case1 < case2){
 		return case1;
 	} else{
 		return case2;
 	}
 }
+
+Word pick_random_words(){
+	int min = 0;
+	int max = 287;	
+	srand(time(NULL));
+	double scaled = (double)rand()/RAND_MAX;
+	int previous_random_index = (max - min +1)*scaled + min;	
+	int random_index = (max - min +1)*scaled + min;	
+	while (random_index == previous_random_index){
+		scaled = (double)rand()/RAND_MAX;
+		random_index = (max - min +1)*scaled + min;	
+	}
+	return words[random_index];
+}
+
 
 
 /********************************************************************************************************************/
@@ -46,7 +61,9 @@ int calculate_num_guesses(int words_length){
 
 int main(int argc, char *argv[]){
 	read_hangman_list();
-	printf("%s\n", words[0].word_b);
+	//printf("%s\n", words[0].word_b);
+	Word test = pick_random_words();
+	printf("%s, %s\n", test.word_a, test.word_b);
 }
 
 //  Compile using:
